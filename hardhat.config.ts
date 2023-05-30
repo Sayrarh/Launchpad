0732e6db5266bdd3512f39bf2d03a1d517213853
Sayrarh <ajaotosinserah@gmail.com>
2023-05-30 19:41:13 +0100

    {config}

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
+
+type HttpNetworkAccountsUserConfig = any;
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.4",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
+      chainId: 44787,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.CELOSCAN_API_KEY
+  }
 };
+
+
 
 export default config;
